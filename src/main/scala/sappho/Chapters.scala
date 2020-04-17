f192c23816bf95f9f6fbb0d3f6d1386d02f18304
softboot <krzyswozny@interia.pl>
2020-04-17 18:23:49 +0200

    Make Chapters extend IndexedSeq

--- src/main/scala/sappho/Chapters.scala
+++ src/main/scala/sappho/Chapters.scala
@@ -1,8 +1,12 @@
 package sappho
 
-trait Chapters extends (Int => Chapter) {
+trait Chapters extends IndexedSeq[Chapter] {
   def count: Int
   def plannedCount: Option[Int]
 
   def apply(i: Int): Chapter
+
+  override def iterator: Iterator[Chapter] = (0 until count).iterator.map(apply)
+
+  final override def length: Int = count
 }
--- src/main/scala/sappho/ao3/Chapters.scala
+++ src/main/scala/sappho/ao3/Chapters.scala
@@ -1,5 +1,7 @@
 package sappho.ao3
 
-trait Chapters extends sappho.Chapters with (Int => Chapter)  {
+trait Chapters extends sappho.Chapters with IndexedSeq[Chapter]  {
   def apply(i: Int): Chapter
+
+  override def iterator: Iterator[Chapter] = (0 until length).iterator.map(apply)
 }
