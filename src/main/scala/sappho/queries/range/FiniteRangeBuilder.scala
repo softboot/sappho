d9bc0f2113581ba860c683ea7df806d2bb128732
softboot <krzyswozny@interia.pl>
2020-04-27 14:20:32 +0200

    Separate definition of IntLHS implicit class into an abstract superclass and an implicit subclass

--- src/main/scala/sappho/queries/range/FiniteRangeBuilder.scala
+++ src/main/scala/sappho/queries/range/FiniteRangeBuilder.scala
@@ -10,12 +10,21 @@ class FiniteRangeBuilder[T, R](lowerBound: Bound[T], rangeBuilder: RangeBuilder[
   }
 }
 object FiniteRangeBuilder {
-  implicit class IntLHS(leftLimit: Int) {
-    def <[R](rangeBuilder: RangeBuilder[Int, R]): FiniteRangeBuilder[Int, R] = {
-      new FiniteRangeBuilder[Int, R](Exclusive(leftLimit), rangeBuilder)
+  abstract class LHS[T] {
+    def leftLimit: T
+
+    def <[R](rangeBuilder: RangeBuilder[T, R]): FiniteRangeBuilder[T, R] = {
+      new FiniteRangeBuilder[T, R](Exclusive(leftLimit), rangeBuilder)
     }
-    def <=[R](rangeBuilder: RangeBuilder[Int, R]): FiniteRangeBuilder[Int, R] = {
-      new FiniteRangeBuilder[Int, R](Inclusive(leftLimit), rangeBuilder)
+    def <=[R](rangeBuilder: RangeBuilder[T, R]): FiniteRangeBuilder[T, R] = {
+      new FiniteRangeBuilder[T, R](Inclusive(leftLimit), rangeBuilder)
     }
+  }
+
+  //For whatever reason, the < and <= operator overloads are not detected if LHS[Int] is used directly.
+  //We need to override them with Int as a concrete, non-parametric type.
+  implicit class IntLHS(val leftLimit: Int) extends LHS[Int] {
+    override def <[R](rangeBuilder: RangeBuilder[Int, R]): FiniteRangeBuilder[Int, R] = super.<(rangeBuilder)
+    override def <=[R](rangeBuilder: RangeBuilder[Int, R]): FiniteRangeBuilder[Int, R] = super.<=(rangeBuilder)
   }
 }
