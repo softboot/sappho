29394d39e0ed336dac2fc59360d6107da08a2227
softboot <krzyswozny@interia.pl>
2020-05-12 17:34:09 +0200

    Move some PageChapter methods to SectionChapter superclass

--- src/main/scala/sappho/ao3/PageChapter.scala
+++ src/main/scala/sappho/ao3/PageChapter.scala
@@ -1,34 +1,17 @@
 package sappho.ao3
 
-import java.time.LocalDate
-
 import com.typesafe.scalalogging.Logger
 import net.ruippeixotog.scalascraper.browser.Browser
-import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
-import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.model._
 import sappho.util.Log._
 
 private class PageChapter(
-    val story: Story,
-    val chapterIndex: Int,
-    val chapterId: Long,
-    page: Document,
-    provider: ChapterInfoProvider)
-  extends Chapter {
-
-  override def title: String = (page >> element("h3.title"))
-    .childNodes
-    .collect { case t: TextNode => t.content.strip() }
-    .filterNot(_.isEmpty)
-    .map(s => s.substring(2))
-    .headOption
-    .getOrElse("Chapter " + (chapterIndex + 1))
-
-  override def postedOn: LocalDate = provider.pollDate(chapterIndex)
-
-  override def paragraphs: Iterable[String] = (page >> texts("div[role=article] p"))
-}
+     story: Story,
+     chapterIndex: Int,
+     override val chapterId: Long,
+     page: Document,
+     provider: ChapterInfoProvider)
+  extends SectionChapter(story, chapterIndex, page.body, provider)
 
 private object PageChapter {
   private val logger = Logger[PageChapter]
---
+++ src/main/scala/sappho/ao3/SectionChapter.scala
@@ -0,0 +1,25 @@
+package sappho.ao3
+
+import java.time.LocalDate
+
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.model._
+
+private class SectionChapter(val story: Story, val chapterIndex: Int, root: Element, provider: ChapterInfoProvider)
+  extends Chapter
+{
+  override def chapterId: Long = provider.pollChapterId(chapterIndex)
+
+  override def postedOn: LocalDate = provider.pollDate(chapterIndex)
+
+  override def title: String = (root >> element("h3.title"))
+    .childNodes
+    .collect { case t: TextNode => t.content.strip() }
+    .filterNot(_.isEmpty)
+    .map(s => s.substring(2))
+    .headOption
+    .getOrElse("Chapter " + (chapterIndex + 1))
+
+  override def paragraphs: Iterable[String] = (root >> texts("div[role=article] p"))
+}
